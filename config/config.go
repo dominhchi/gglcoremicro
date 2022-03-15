607937949278dfa725c33c3e729228524d2bcc25
Minh Chi <99shadowdragon@gmail.com>
2022-03-15 10:08:09 +0700

    authorize

--- config/config.go
+++ config/config.go
@@ -1,23 +1,20 @@
 package config
 
-import "fmt"
+import (
+	"github.com/kelseyhightower/envconfig"
+	"log"
+)
+
+func init() {
+	err := envconfig.Process("", &Cfg)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
 
 type Config struct {
-	Host     string `envconfig:"DB_HOST_SERVICE" default:"localhost"`
-	Port     int    `envconfig:"DB_PORT_SERVICE" default:"5432"`
-	User     string `envconfig:"DB_USER_SERVICE" default:"postgres"`
-	DBName   string `envconfig:"DB_NAME_SERVICE" default:"service"`
-	Password string `envconfig:"DB_PASSWORD_SERVICE" default:"postgres"`
-
 	MediaURL  string `envconfig:"MEDIA_URL" default:"https://housebuildingstorage.blob.core.windows.net/media/"`
 	SecretKey string `envconfig:"SECRET_KEY" default:"@ytbjco&cb^7hh!z3$dxih9+r$byc)s(!s-$_@f(cdt7))p4)x"`
 }
 
 var Cfg Config
-
-func (cfg *Config) DbDSN() string {
-	return fmt.Sprintf(
-		"host=%s user=%s password=%s dbname=%s port=%d sslmode=disable TimeZone=Asia/Saigon",
-		cfg.Host, cfg.User, cfg.Password, cfg.DBName, cfg.Port,
-	)
-}
--- go.mod
+++ go.mod
@@ -9,6 +9,7 @@ require (
 	github.com/go-playground/validator/v10 v10.10.1
 	github.com/golang-jwt/jwt v3.2.2+incompatible
 	github.com/google/uuid v1.3.0
+	github.com/kelseyhightower/envconfig v1.4.0
 	github.com/labstack/echo/v4 v4.7.1
 	github.com/vektah/gqlparser/v2 v2.2.0
 )
